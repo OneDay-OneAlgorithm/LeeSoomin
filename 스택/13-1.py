400c67687dba246b56e029c6b66d53109ed8d34f
intersoom <sallybig@naver.com>
2024-01-13 20:21:59 +0900

    스택 추가 문제: 오큰수

--- 스택/13-1.py
+++ 스택/13-1.py
@@ -1,26 +1,16 @@
 # 오큰수
-
 import sys
-import copy
-from collections import deque
+n = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+answer = [-1] * n
+stack = []
 
-N = int(sys.stdin.readline())
-A = deque(list(map(int, sys.stdin.readline().split(' '))))
-result = []
 
-for _ in range(len(A)):
-  a = A.popleft()
-  stack = deque(copy.deepcopy(A))
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
 
-  while stack:
-    if stack[0] > a:
-      result.append(stack[0])
-      break
-    else:
-      stack.popleft()
-  
-  if len(stack) == 0:
-    result.append(-1)
 
-for r in result:
-  print(r, end=' ')
+print(*answer)
